6760e6bc18e53fadfbb3c74f2150270464c04923
Ronny Springer <springer.ronny@gmail.com>
2021-09-29 09:23:41 +0200

    consume text ellipses

--- src/components/InboxList.js
+++ src/components/InboxList.js
@@ -1,21 +1,28 @@
 import React from "react";
+import Text from "./Text";
 
 const InboxList = ({ inbox = [] }) => {
   return inbox.length > 0 ? (
     <div className="card">
-      {inbox.map(({ nummer, datum, absender, betreff, anmerkung }, index) => (
+      {inbox.map(({ id, datum, absender, betreff, anmerkung }) => (
         <div
           className="card-body"
-          id={`inbox-row-${index}`}
-          key={`inbox-row-${index}`}
+          id={`inbox-row-${id}`}
+          key={`inbox-row-${id}`}
           style={{ borderBottom: "1px solid lightgray" }}
         >
           <div className="row">
-            <div className="col-1">{nummer}</div>
+            <div className="col-1">{id}</div>
             <div className="col-2">{datum}</div>
-            <div className="col-3 text-ellipsis">{absender}</div>
-            <div className="col-3 text-ellipsis">{betreff}</div>
-            <div className="col-2 text-ellipsis">{anmerkung}</div>
+            <div className="col-3 pe-3">
+              <Text text={absender} />
+            </div>
+            <div className="col-3 ps-4 pe-4">
+              <Text text={betreff} />
+            </div>
+            <div className="col-2 ps-4 pe-4">
+              <Text text={anmerkung} />
+            </div>
           </div>
         </div>
       ))}

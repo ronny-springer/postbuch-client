1a0b99b7db0686b5ba132969688f5c711ef00027
Ronny Springer <springer.ronny@gmail.com>
2021-09-29 09:22:39 +0200

    add text ellipses

---
+++ src/components/Text.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Text.modul.css";
+
+const Text = ({ text }) => {
+  return text.length > 0 ? <p className="text-ellipsis">{text}</p> : <p>-</p>;
+};
+
+export default Text;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as InboxForm } from "./InboxForm";
 export { default as InboxList } from "./InboxList";
 export { default as Navigation } from "./Navigation";
+export { default as Text } from "./Text";

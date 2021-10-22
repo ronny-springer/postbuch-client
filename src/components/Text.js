4176c545f58e963113ad77b1b0bc850c5119dbbe
Ronny Springer <springer.ronny@gmail.com>
2021-10-22 10:31:51 +0200

    Create Text Component

--- src/components/Text.js
+++ src/components/Text.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Text.modul.css";
 
 const Text = ({ text = "" }) => {
-  return text.length > 0 ? <p className="text-ellipsis">{text}</p> : <p>-</p>;
+  return text.length > 0 ? (
+    <p className="text-ellipsis">{text}</p>
+  ) : (
+    <p className="text-empty">-</p>
+  );
 };
 
 export default Text;

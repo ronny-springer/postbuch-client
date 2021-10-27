c67e5f1000526d55f1567fc7f8c4f127887a382a
Ronny Springer <springer.ronny@gmail.com>
2021-10-27 14:36:38 +0200

    refactor Text

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,12 +1,20 @@
 import React from "react";
-import "./Text.modul.css";
 
-const Text = ({ text = "" }) => {
-  return text.length > 0 ? (
-    <p className="text-ellipsis">{text}</p>
-  ) : (
-    <p className="text-empty">-</p>
-  );
+const Text = ({ currency = "", children }) => {
+  if (!children) return <p className="m-0">-</p>;
+
+  if (currency.length && currency === "Euro")
+    return (
+      <p className="text-truncate m-0">
+        {children.toLocaleString("de-DE", {
+          style: "currency",
+          currency: "EUR",
+          minimumFractionDigits: 2,
+        })}
+      </p>
+    );
+
+  return <p className="text-truncate m-0">{children}</p>;
 };
 
 export default Text;

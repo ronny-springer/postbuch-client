4fcd1990b069af51a53a0f4df8a0e383d5b2e86a
Ronny Springer <springer.ronny@gmail.com>
2021-10-27 11:03:53 +0200

    show disabled entries in list

--- src/components/InboxList.js
+++ src/components/InboxList.js
@@ -4,9 +4,7 @@ import { AiOutlineDelete } from "react-icons/ai";
 import Text from "./Text";
 
 const InboxList = ({ inbox = [], onEdit, onDelete }) => {
-  const displayableInbox = inbox.filter(({ isDisabled }) => !isDisabled) || [];
-
-  if (!displayableInbox.length)
+  if (!inbox.length)
     return (
       <div className="list-group">
         <p className="list-group-item alert alert-light mb-0">
@@ -17,55 +15,86 @@ const InboxList = ({ inbox = [], onEdit, onDelete }) => {
 
   return (
     <div className="list-group">
-      {displayableInbox.map(
-        ({ id, counter, datum, absender, type, recipient }) => (
-          <div
-            className="list-group-item pt-0 pb-0"
-            id={`inbox-row-${id}`}
-            key={`inbox-row-${id}`}
-            style={{ borderBottom: "1px solid lightgray" }}
-          >
-            <div className="row d-flex align-items-center">
-              <div className="col-1">{counter}</div>
-              <div className="col-2">{datum}</div>
-              <div className="col-3">
-                <Text text={absender} />
+      {inbox.map(
+        ({ id, counter, datum, absender, type, recipient, isDisabled }) => {
+          if (isDisabled) {
+            return (
+              <div
+                className="list-group-item pt-0 pb-0"
+                id={`inbox-row-${id}`}
+                key={`inbox-row-${id}`}
+                style={{
+                  borderBottom: "1px solid lightgray",
+                  lineHeight: "40px",
+                  background: "rgb(249 249 249)",
+                }}
+              >
+                <div className="row d-flex align-items-center">
+                  <div className="col-1">
+                    <i style={{ color: "rgb(207 207 207)" }}>{counter}</i>
+                  </div>
+                  <div className="col-2">
+                    <i style={{ color: "rgb(207 207 207)" }}>{datum}</i>
+                  </div>
+                  <div className="col">
+                    <i>
+                      <Text text="Eintrag entfernt!" />
+                    </i>
+                  </div>
+                </div>
               </div>
-              <div className="col-2" style={{ paddingLeft: "24px" }}>
-                <Text text={type} />
-              </div>
-              <div className="col-3" style={{ paddingLeft: "116px" }}>
-                <Text text={recipient} />
-              </div>
-              <div className="col-1">
-                <button
-                  className={cx("btn", "btn-light")}
-                  onClick={(event) => {
-                    event.preventDefault();
+            );
+          }
 
-                    onDelete([
-                      ...new Map(
-                        [
-                          ...inbox,
-                          {
-                            id,
-                            counter: null,
-                            datum,
-                            absender,
-                            type,
-                            isDisabled: true,
-                          },
-                        ].map((item) => [item.id, item])
-                      ).values(),
-                    ]);
-                  }}
-                >
-                  <AiOutlineDelete />
-                </button>
+          return (
+            <div
+              className="list-group-item pt-0 pb-0"
+              id={`inbox-row-${id}`}
+              key={`inbox-row-${id}`}
+              style={{ borderBottom: "1px solid lightgray" }}
+            >
+              <div className="row d-flex align-items-center">
+                <div className="col-1">{counter}</div>
+                <div className="col-2">{datum}</div>
+                <div className="col-3">
+                  <Text text={absender} />
+                </div>
+                <div className="col-2" style={{ paddingLeft: "24px" }}>
+                  <Text text={type} />
+                </div>
+                <div className="col-3" style={{ paddingLeft: "116px" }}>
+                  <Text text={recipient} />
+                </div>
+                <div className="col-1">
+                  <button
+                    className={cx("btn", "btn-light")}
+                    onClick={(event) => {
+                      event.preventDefault();
+
+                      onDelete([
+                        ...new Map(
+                          [
+                            ...inbox,
+                            {
+                              id,
+                              counter,
+                              datum,
+                              absender,
+                              type,
+                              isDisabled: true,
+                            },
+                          ].map((item) => [item.id, item])
+                        ).values(),
+                      ]);
+                    }}
+                  >
+                    <AiOutlineDelete />
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )
+          );
+        }
       )}
     </div>
   );
--- src/components/OutboxList.js
+++ src/components/OutboxList.js
@@ -4,10 +4,7 @@ import { AiOutlineDelete } from "react-icons/ai";
 import Text from "./Text";
 
 const OutboxList = ({ outbox = [], onEdit, onDelete }) => {
-  const displayableOutbox =
-    outbox.filter(({ isDisabled }) => !isDisabled) || [];
-
-  if (!displayableOutbox.length)
+  if (!outbox.length)
     return (
       <div className="list-group">
         <p className="list-group-item alert alert-light mb-0">
@@ -18,62 +15,102 @@ const OutboxList = ({ outbox = [], onEdit, onDelete }) => {
 
   return (
     <div className="list-group">
-      {displayableOutbox.map(
-        ({ id, counter, datum, empfänger, type, sendung, preis }) => (
-          <div
-            className="list-group-item pt-0 pb-0"
-            id={`inbox-row-${id}`}
-            key={`outbox-row-${id}`}
-            style={{ borderBottom: "1px solid lightgray" }}
-          >
-            <div className="row d-flex align-items-center">
-              <div className="col-1">{counter}</div>
-              <div className="col-2">{datum}</div>
-              <div className="col-3">
-                <Text text={empfänger} />
+      {outbox.map(
+        ({
+          id,
+          counter,
+          datum,
+          empfänger,
+          type,
+          sendung,
+          preis,
+          isDisabled,
+        }) => {
+          if (isDisabled) {
+            return (
+              <div
+                className="list-group-item pt-0 pb-0"
+                id={`inbox-row-${id}`}
+                key={`inbox-row-${id}`}
+                style={{
+                  borderBottom: "1px solid lightgray",
+                  lineHeight: "40px",
+                  background: "rgb(249 249 249)",
+                }}
+              >
+                <div className="row d-flex align-items-center">
+                  <div className="col-1">
+                    <i style={{ color: "rgb(207 207 207)" }}>{counter}</i>
+                  </div>
+                  <div className="col-2">
+                    <i style={{ color: "rgb(207 207 207)" }}>{datum}</i>
+                  </div>
+                  <div className="col">
+                    <i>
+                      <Text text="Eintrag entfernt!" />
+                    </i>
+                  </div>
+                </div>
               </div>
-              <div className="col-2" style={{ paddingLeft: "24px" }}>
-                <Text text={type} />
-              </div>
-              <div className="col-3" style={{ paddingLeft: "32px" }}>
-                <Text
-                  text={`${sendung} (${preis.toLocaleString("de-DE", {
-                    style: "currency",
-                    currency: "EUR",
-                    minimumFractionDigits: 2,
-                  })})`}
-                />
-              </div>
-              <div className="col-1">
-                <button
-                  className={cx("btn", "btn-light")}
-                  onClick={(event) => {
-                    event.preventDefault();
-                    onDelete([
-                      ...new Map(
-                        [
-                          ...outbox,
-                          {
-                            id,
-                            counter: null,
-                            datum,
-                            empfänger,
-                            type,
-                            sendung,
-                            preis,
-                            isDisabled: true,
-                          },
-                        ].map((item) => [item.id, item])
-                      ).values(),
-                    ]);
-                  }}
-                >
-                  <AiOutlineDelete />
-                </button>
+            );
+          }
+
+          return (
+            <div
+              className="list-group-item pt-0 pb-0"
+              id={`inbox-row-${id}`}
+              key={`outbox-row-${id}`}
+              style={{ borderBottom: "1px solid lightgray" }}
+            >
+              <div className="row d-flex align-items-center">
+                <div className="col-1">{counter}</div>
+                <div className="col-2">{datum}</div>
+                <div className="col-3">
+                  <Text text={empfänger} />
+                </div>
+                <div className="col-2" style={{ paddingLeft: "24px" }}>
+                  <Text text={type} />
+                </div>
+                <div className="col-3" style={{ paddingLeft: "32px" }}>
+                  <Text
+                    text={`${sendung} (${preis.toLocaleString("de-DE", {
+                      style: "currency",
+                      currency: "EUR",
+                      minimumFractionDigits: 2,
+                    })})`}
+                  />
+                </div>
+                <div className="col-1">
+                  <button
+                    className={cx("btn", "btn-light")}
+                    onClick={(event) => {
+                      event.preventDefault();
+                      onDelete([
+                        ...new Map(
+                          [
+                            ...outbox,
+                            {
+                              id,
+                              counter,
+                              datum,
+                              empfänger,
+                              type,
+                              sendung,
+                              preis,
+                              isDisabled: true,
+                            },
+                          ].map((item) => [item.id, item])
+                        ).values(),
+                      ]);
+                    }}
+                  >
+                    <AiOutlineDelete />
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )
+          );
+        }
       )}
     </div>
   );
